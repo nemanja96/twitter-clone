ad1d46fc48717b1c318278f6005c95114e22fcee
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-07-31 15:05:37 +0200

    Finished the feed component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
 
       <main>
         <Sidebar />
+        <Feed />
       </main>
      
     </div>
